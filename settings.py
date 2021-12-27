88a725b1e03e88a3b76c121f65213067678a8a28
w1png <wonepng@gmail.com>
2021-12-27 23:07:44 +0300

    Add --debug_mode --color --borderwidth

--- settings.py
+++ settings.py
@@ -1,16 +1,55 @@
 from configparser import ConfigParser
 from argparse import ArgumentParser
 from configparser import ConfigParser
+import re
+
+class Settings:
+    def __init__(self) -> None:
+        conf = ConfigParser()
+        conf.read("config.ini", encoding="utf8")
+        
+    def __set_setting(self, category, subcategory, value):
+        self.conf.set(category, subcategory, value)
+        with open("config.ini", 'w') as config:
+            self.conf.write(config)
+            
+    def get_token(self):
+        return self.conf["main_settings"]["token"]
+    
+    def set_token(self, value):
+        self.__set_setting("main_settings", "token", value)
+    
+    def get_time(self):
+        return self.conf["main_settings"]["time"]
+    
+    def set_time(self, value):
+        self.__set_setting("main_settings", "time", value)
+        
+    def is_debug(self):
+        return self.conf["main_settings", "debug_mode"] == "True"
+    
+    def set_debugmode(self, value):
+        self.__set_setting("main_settings", "debug_mode", value)
 
 if __name__ == "__main__":
-    conf = ConfigParser()
-    conf.read("config.ini", encoding="utf8")
-    
     parser = ArgumentParser(description="Allows you to change the bot's settings in the CLI.")
-    parser.add_argument("-n", "--notificationtime", help="time in the \"hours:minutes\" format i.e. \"15:21\".")
     parser.add_argument("-t", "--token", help="the bot's token.")
+    parser.add_argument("-d", "--debug_mode", help="True/False to enable/disable debug mode.")
+    parser.add_argument("-n", "--notificationtime", help="time in the \"hours:minutes\" format i.e. \"15:21\".")
+    parser.add_argument("-c", "--color", help="the color of the graph in hex format i.e. \"#D3D3D3\".")
+    parser.add_argument("-b", "--borderwidth", help="border width of the graph.")
+
+    if parser.parse_args().debug_mode:
+        settings = Settings()
+        debug_mode = parser.parse_args().debug_mode
+        if debug_mode in ["True", "False"]:
+            print(f"Debug mode was updated from \"{settings.get_debug_mode()}\" to \"{debug_mode}\"")
+            settings.set_debug_mode(debug_mode)
+        else:
+            print(f"\"{debug_mode}\" is not a valid value.", parser.print_help(), sep="\n")
 
     if parser.parse_args().notificationtime:
+        settings = Settings()
         try:
             time = parser.parse_args().notificationtime.split(":")
             if len(time[0]) == len(time[1]) == 2 and time[0].isalnum() and time[1].isalnum():
@@ -21,14 +60,27 @@ if __name__ == "__main__":
             valid = False
         if valid:
             time = parser.parse_args().notificationtime
-            print(f"Notification time was updated from \"{conf['main_settings']['time']}\" to \"{time}\".")
-            conf.set("main_settings", "time", time)
-            with open("config.ini", 'w') as config:
-                conf.write(config)
+            print(f"Notification time was updated from \"{settings.get_time()}\" to \"{time}\".")
+            settings.set_time(time)
         else:
             parser.print_help()
     if parser.parse_args().token:
+        settings = Settings()
         print("Your bot's token was updated.")
-        conf.set("main_settings", "token", parser.parse_args().token)
-        with open("config.ini", "w") as config:
-            conf.write(config)
+        settings.set_token(parser.parse_args().token)
+    if parser.parse_args().color:
+        settings = Settings()
+        color = parser.parse_args().color
+        if re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', color): # No idea how this works, taken from https://stackoverflow.com/questions/30241375/python-how-to-check-if-string-is-a-hex-color-code
+            print(f"Graph color was changed from \"{settings.get_graphcolor()}\" to \"{color}\".")
+            settings.set_graphcolor(f"\"color\"")
+        else:
+            print(f"\"{color}\" is not a valid hex color.", parser.print_help(), sep="\n")
+    if parser.parse_args().border:
+        settings = Settings()
+        border = parser.parse_args().border
+        if border.isalnum():
+            print(f"Border width was changed from \"{settings.get_border()}\" to \"{border}\".")
+            settings.set_border(border)
+        else:
+            print(f"\"{border}\" is not a number!", parser.print_help(), sep="\n")
